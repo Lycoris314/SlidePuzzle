43ee2fdf2a6f895ffed7d513f7ae65150534b0ca
Lycoris314 <159458009+Lycoris314@users.noreply.github.com>
2024-05-23 19:47:45 +0900

    PHP消去

--- slidepuzzle.js
+++ slidepuzzle.js
@@ -1,50 +1,46 @@
-
 $(() => {
+    // $.ajax({
+    //     url: "imglist.php",
+    //     type: "get",
+    //     data: "exclude=default.jpg",
+    //     //dataType: "json",
+    //     cache: false,
+    // }).done((data) => {
 
-    $.ajax({
-        url: "imglist.php",
-        type: "get",
-        data: "exclude=default.jpg",
-        //dataType: "json",
-        cache: false,
-    }).done((data) => {
+    //     console.log("データ取得に成功しました。");
+    //     //console.log(data);
 
-        console.log("データ取得に成功しました。");
-        //console.log(data);
+    //     let imageList = JSON.parse(data);
 
-        let imageList = JSON.parse(data);
+    //     for (let image of imageList.values()) {
+    //         let div = $("<div class=frame>");
 
-        for (let image of imageList.values()) {
-            let div = $("<div class=frame>");
+    //         div.append($("<img>").attr("src", "img/thumbnail/" + image))
+    //             .attr("data-imgPath", "img/" + image);
 
-            div.append($("<img>").attr("src", "img/thumbnail/" + image))
-                .attr("data-imgPath", "img/" + image);
+    //         $(".image_list").append(div);
+    //     }
 
-            $(".image_list").append(div);
-        }
+    //     $(".frame").on("click", function () {
 
-        $(".frame").on("click", function () {
+    //         $(".frame").removeClass("on");
+    //         $(this).addClass("on");
 
-            $(".frame").removeClass("on");
-            $(this).addClass("on");
+    //         $(".img-path").val($(this).attr("data-imgPath"))
 
-            $(".img-path").val($(this).attr("data-imgPath"))
+    //         $("<img>").attr("src", $(this).attr("data-imgPath")); //プリロード
+    //     })
 
-            $("<img>").attr("src", $(this).attr("data-imgPath")); //プリロード
-        })
-
-    }).fail(() => {
-        console.log("データ取得に失敗しました。")
-    })
+    // }).fail(() => {
+    //     console.log("データ取得に失敗しました。")
+    // })
 
     //起動時
     let panemane = "";
     createPanels();
     addPanelEvent();
 
-
     function createPanels(shuffle) {
-
         $(".field").empty();
 
         const width = $("#width").val();
@@ -54,41 +50,48 @@ $(() => {
         panemane = new PanelManager(width, height, imgPath);
 
         for (let panel of panemane.getViewPanels()) {
-
             $(".field").append(panel);
         }
-        if (shuffle == "shuffle") { panemane.shufflePanels(); }
+        if (shuffle == "shuffle") {
+            panemane.shufflePanels();
+        }
     }
 
     function addPanelEvent() {
-
         $(".panel").on("click", function () {
-
             if (!$(".empty").hasClass("visible")) {
-
-                count += panemane.movePanel($(this).data("now-y"), $(this).data("now-x"));
+                count += panemane.movePanel(
+                    $(this).data("now-y"),
+                    $(this).data("now-x")
+                );
                 $(".count").text(count);
             }
-        })
+        });
     }
 
     //シャッフルボタンを押す
     $(".shuffle").on("click", () => {
-
         createPanels("shuffle");
         addPanelEvent();
         numbering();
         resetCount();
     });
 
-
     //サイドメニュー表示ボタンクリック
     $(".arrow").on("click", () => {
-
         $(".side").toggleClass("show");
         $(".arrow").toggleClass("lt gt");
         $(".gray_cover").toggleClass("on");
-    })
+    });
+
+    $(".frame").on("click", function () {
+        $(".frame").removeClass("on");
+        $(this).addClass("on");
+
+        $(".img-path").val($(this).attr("data-imgPath"));
+
+        $("<img>").attr("src", $(this).attr("data-imgPath")); //プリロード
+    });
 
     //リセットして反映ボタンクリック
     $("form").on("submit", (e) => {
@@ -103,20 +106,15 @@ $(() => {
 
         numbering();
         resetCount();
-
-    })
+    });
 
     function numbering() {
         if ($("#radio1").prop("checked")) {
-            $(".panel").removeClass("no-num white")
-
+            $(".panel").removeClass("no-num white");
         } else if ($("#radio2").prop("checked")) {
-            $(".panel").removeClass("no-num")
-                .addClass("white")
-
+            $(".panel").removeClass("no-num").addClass("white");
         } else if ($("#radio3").prop("checked")) {
-            $(".panel").removeClass("white")
-                .addClass("no-num")
+            $(".panel").removeClass("white").addClass("no-num");
         }
     }
 
@@ -127,16 +125,6 @@ $(() => {
         $(".count").text(0);
     }
 
-
     //テキスト選択禁止
     document.onselectstart = () => false;
-
-
-
-
-
-
-
-
-
-})
+});
